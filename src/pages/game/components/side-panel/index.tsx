b1a5460cfba582f9c1330c6deea0f636ef9ed586
Dustin Craig <dustincraig@MacBook-Pro.attlocal.net>
2023-08-04 00:40:07 -0400

    Change turn text based on current turn value

--- src/pages/game/components/side-panel/index.tsx
+++ src/pages/game/components/side-panel/index.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 import FlexBox from "../../../../components/flex-box";
-import { Paper, Typography, Divider } from "@mui/material";
+import { Paper, Typography, Divider, Stack } from "@mui/material";
+import { useGame } from "../../../../hooks/useGame";
 
 export default () => {
+  const {
+    state: { currentTurn, playerId },
+  } = useGame();
+
   return (
     <FlexBox>
       <Paper sx={{ width: "100%", height: "100%", textAlign: "center", p: 2 }}>
         <Typography variant="h4">Connect 4</Typography>
         <Divider />
+        <Stack
+          sx={{
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          direction={"column"}
+        >
+          <Typography variant="h5">
+            {currentTurn === playerId ? "Your turn" : "Other players turn"}
+          </Typography>
+        </Stack>
       </Paper>
     </FlexBox>
   );
