5c44025196eac0f16ee4507ddf31d3b4404948dc
joeylemon <joeyclemon@gmail.com>
2023-10-06 08:08:01 -0400

    change connection to https

--- src/connection.tsx
+++ src/connection.tsx
@@ -5,7 +5,7 @@ import { GameActionType } from "./types/game";
 import { useGame } from "./hooks/useGame";
 import { io } from "socket.io-client";
 
-const socket = io("http://jlemon.org:4533");
+const socket = io("https://jlemon.org:4533");
 
 type Props = {
   children: ReactNode;
